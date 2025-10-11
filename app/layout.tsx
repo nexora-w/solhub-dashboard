8a132bd6c31ea3f6c9bd1cafd18ec2b55e514a9c
nexora-w <kitchi734@gmail.com>
2025-10-12 00:42:03 +0900

    feat: change metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,9 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'Userhub Dashboard',
+  description: 'Userhub Dashboard',
+  generator: 'Tamadoge',
 }
 
 export default function RootLayout({
